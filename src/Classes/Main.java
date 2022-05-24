ca0cacedd9b6be506b5ee084b7cea243e1a19d4f
Jose Rubin <jmanuelrubin1@gmail.com>
2022-05-23 20:48:01 -0400

    Variables

--- src/Classes/Main.java
+++ src/Classes/Main.java
@@ -18,19 +18,27 @@ public class Main {
      * @param args the command line arguments
      */
     
-    public static volatile int countdown=30;
-    public static volatile int maxCamaras=20;
-    public static volatile int maxBotones=45;
-    public static volatile int maxPantallas=40;
-    
+    public static volatile int countdownPlantaJ=30;
+    public static volatile int countdownPlantaM=30;
     public static Semaphore sem1 = new Semaphore(5);
+    
+   //Variables Rubin
     public static JefeJ jefeJ;
     public static GerenteJ gerenteJ;
     public static EnsambladoresJ assemblerJ;
-    public static ProductoresBotonesJ producBotons;
+    public static ProductoresBotonesJ producBotonesJ;
     public static ProductoresPinesJ producPins;
-    public static ProductoresCamarasJ producCams;
-    public static ProductoresPantallasJ producScreens;
+    public static ProductoresCamarasJ producCamsJ;
+    public static ProductoresPantallasJ producPantJ;
+    
+    //Variables Massimo
+//    public static JefeM jefeM;
+//    public static GerenteM gerenteM;
+//    public static EnsambladoresM assemblerM;
+//    public static ProductoresBotonesM producBotonesM;
+//    public static ProductoresPinesM producPinsM;
+//    public static ProductoresCamarasM producCamsM;
+//    public static ProductoresPantallasM producPantM;
     
     public static void main(String[] args) {
         // TODO code application logic here
